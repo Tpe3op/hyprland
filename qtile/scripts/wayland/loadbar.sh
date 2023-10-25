373dfe5078edc7ca9f87c6ffc81f146e52055cf0
Stephan Raabe <mail@stephan-raabe.de>
2023-10-25 16:30:38 +0200

    Update comments

--- qtile/scripts/wayland/loadbar.sh
+++ qtile/scripts/wayland/loadbar.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+#  _                 _ _                 
+# | | ___   __ _  __| | |__   __ _ _ __  
+# | |/ _ \ / _` |/ _` | '_ \ / _` | '__| 
+# | | (_) | (_| | (_| | |_) | (_| | |    
+# |_|\___/ \__,_|\__,_|_.__/ \__,_|_|    
+#                                        
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+# ----------------------------------------------------- 
+# Check if information about the bar exists in .cache
+# If not create it
+# ----------------------------------------------------- 
 if [ ! -f ~/.cache/.qtile_bar_wayland.sh ]; then
     touch ~/.cache/.qtile_bar_wayland.sh
     echo "qtile" > ~/.cache/.qtile_bar_wayland.sh
     echo ".qtile_bar_wayland.sh created"
 fi    
 
+# ----------------------------------------------------- 
+# Load status bar information
+# ----------------------------------------------------- 
 bar=$(cat ~/.cache/.qtile_bar_wayland.sh)
+
+# ----------------------------------------------------- 
+# Load status bar based on loaded information
+# ----------------------------------------------------- 
 if [ $bar == "qtile" ]; then
     killall waybar
     sleep 1
